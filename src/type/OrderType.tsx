47625641e28e82d4540995e6b41d0c38189e0fa1
sankar <95sankar@gmail.com>
2024-05-07 08:24:09 +0530

    26

---
+++ src/type/OrderType.tsx
@@ -0,0 +1,26 @@
+interface OrderItem {
+  product: string; // You can use the type that represents your Product ID
+  color?: string; // You can use the type that represents your Color ID
+  variation?: string; // You can use the type that represents your Variation ID
+  quantity: number;
+  price: number;
+}
+
+export interface IOrder {
+  _id: string;
+  user?: string;
+  items: OrderItem[];
+  name?: string;
+  total: number;
+  totalItem: number;
+  voucher?: string;
+  taxFee: number;
+  shippingAddress: string;
+  shippingCost: number;
+  phoneNumber?: string;
+  email?: string;
+  paymentMethod: string;
+  status: string;
+  createDate?: string;
+  modifyDate?: string;
+}
